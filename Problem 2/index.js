dc58fd060fff309d835c767265fed8aa0d131829
Firda Ayu Hassanah <131138947+userfirdaayuuu@users.noreply.github.com>
2023-10-01 00:52:49 +0700

    Update problem 2

--- Problem 2/index.js
+++ Problem 2/index.js
@@ -1,51 +1,54 @@
-function getProductsAmount(productsArr) {
-  // Write your code here
-  // EXPECTIATION
-  // Input from customer.products
-  // [
-  //   ["Asus ROG", 2],
-  //   ["Lenovo Legion", 3],
-  // ],
-  // Result
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
+function getProductAmount(products) {
+  const listProductAmount = {};
+  for (var i = 0; i < products.length; i++) {
+    const product = products[i];
+    const nameProduct = product[0];   
+    const amountProduct = product[1]; 
+    listProductAmount[nameProduct] = amountProduct;
+  }
+  return listProductAmount;
 }
 
-function getTotalPrice(productsObj) {
-  const listProduct = [
-    ["Asus ROG", 25000000],
-    ["Lenovo Legion", 22000000],
-    ["Zyrex 1945", 7000000],
-    ["HP Omen", 20000000],
-    ["Acer Predator", 21000000],
-  ];
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
-  // Result
-  // 121000000 // NUMBER
+function getTotalPrice(listProductAmount) { 
+  const prices = {
+    "Asus ROG": 25000000,
+    "Lenovo Legion": 22000000,
+    "Zyrex 1945": 7000000,
+    "HP Omen": 20000000,
+    "Acer Predator": 21000000,
+  };
+  let totalPrice = 0;
+  for (const nameProduct in listProductAmount) {
+    if (listProductAmount.hasOwnProperty(nameProduct)) {
+      const qtyProduct = listProductAmount[nameProduct];
+      totalPrice += prices[nameProduct] * qtyProduct;
+    }
+  }
+  return totalPrice;
 }
 
 function getDiscount(memberStatus, totalPrice) {
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // memberStatus = true
-  // totalPrice = 121000000
-  // Result
-  // 92800000 // NUMBER
+  const percenDiscount = 20;
+  if (memberStatus) {
+    const amountDiscount = (percenDiscount / 100) * totalPrice;
+    return totalPrice - amountDiscount;
+  } else {
+    return totalPrice;
+  }
 }
 
 function shoppingTeros(customer) {
-  // write your code here pakai function yg sudah dibuat diatas didalam sini
-  // EXPECTATION Return berupa string sesuai contoh dibawah
-  // apabila member maka panggil dia pelanggan setia
+  const products = customer.products;
+  const listProductAmount = getProductAmount(products);
+  const totalPrice = getTotalPrice(listProductAmount);
+  const memberStatus = customer.member;
+  const finalPrice = getDiscount(memberStatus, totalPrice);
+
+  if (memberStatus) {
+    return `Hai pelanggan setia ${customer.name}! Total Harga yang harus kamu bayar adalah Rp ${finalPrice}`;
+  } else {
+    return `Hai ${customer.name}! Total Harga yang harus kamu bayar adalah Rp ${totalPrice}`;
+  }
 }
 
 let customer1 = {
@@ -74,6 +77,6 @@ let customer3 = {
 };
 
 // TEST CASES
-console.log(shoppingTeros(customer1)); // Hai pelanggan setia Fajrin! Total Harga yang harus kamu bayar adalah Rp 92800000
-console.log(shoppingTeros(customer2)); // Hai Fadlila! Total Harga yang harus kamu bayar adalah Rp 118000000
+console.log(shoppingTeros(customer1)); // Hai pelanggan setia Fajrin! Total Harga yang harus kamu bayar adalah Rp 92600000
+console.log(shoppingTeros(customer2)); // Hai Fadlila! Total Harga yang harus kamu bayar adalah Rp 116000000
 console.log(shoppingTeros(customer3)); // Hai pelanggan setia Jetly! Total Harga yang harus kamu bayar adalah Rp 32000000

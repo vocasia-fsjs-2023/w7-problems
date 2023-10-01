ce62d1a5a76910ec69844e392195fd260dccee74
Izzuddin AlFatah <68839136+AlFatah743@users.noreply.github.com>
2023-10-01 22:12:44 +0700

    Problem 2 Week 7
    Problem 2 Week 7

--- Problem 2/index.js
+++ Problem 2/index.js
@@ -1,79 +1,60 @@
-function getProductsAmount(productsArr) {
-  // Write your code here
-  // EXPECTIATION
-  // Input from customer.products
-  // [
-  //   ["Asus ROG", 2],
-  //   ["Lenovo Legion", 3],
-  // ],
-  // Result
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
+function getProductAmount(products) {
+  const listProductAmount = {};
+
+  for (const product of products) {
+    const [productName, quantity] = product;
+    listProductAmount[productName] = (listProductAmount[productName] || 0) + quantity;
+  }
+
+  return listProductAmount;
 }
 
-function getTotalPrice(productsObj) {
-  const listProduct = [
-    ["Asus ROG", 25000000],
-    ["Lenovo Legion", 22000000],
-    ["Zyrex 1945", 7000000],
-    ["HP Omen", 20000000],
-    ["Acer Predator", 21000000],
-  ];
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
-  // Result
-  // 121000000 // NUMBER
+function getTotalPrice(listProductAmount) {
+  const productList = {
+    "Asus ROG": 25000000,
+    "Lenovo Legion": 22000000,
+    "HP Omen": 20000000,
+    "Acer Predator": 21000000,
+  };
+
+  let totalPrice = 0;
+
+  for (const productName in listProductAmount) {
+    const quantity = listProductAmount[productName];
+    const price = productList[productName] || 0;
+    totalPrice += quantity * price;
+  }
+
+  return totalPrice;
 }
 
-function getDiscount(memberStatus, totalPrice) {
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // memberStatus = true
-  // totalPrice = 121000000
-  // Result
-  // 92800000 // NUMBER
+function getDiscount(statusMember, totalPrice) {
+  const discountPercentage = statusMember ? 20 : 0;
+  const discountAmount = (discountPercentage / 100) * totalPrice;
+  const totalPriceAfterDiscount = totalPrice - discountAmount;
+
+  return totalPriceAfterDiscount;
 }
 
 function shoppingTeros(customer) {
-  // write your code here pakai function yg sudah dibuat diatas didalam sini
-  // EXPECTATION Return berupa string sesuai contoh dibawah
-  // apabila member maka panggil dia pelanggan setia
+  const { name, products, member } = customer;
+  
+  const listProductAmount = getProductAmount(products);
+  const totalPrice = getTotalPrice(listProductAmount);
+  const totalPriceAfterDiscount = getDiscount(member, totalPrice);
+
+  const greeting = member ? Hai pelanggan setia ${name}! : Hai ${name}!;
+
+  return ${greeting} Total Harga yang harus kamu bayar adalah Rp ${totalPriceAfterDiscount};
 }
 
-let customer1 = {
-  name: "Fajrin",
-  products: [
-    ["Asus ROG", 2],
-    ["Lenovo Legion", 3],
-  ],
+// Contoh:
+const customer = {
+  name: "Amir",
+  products: [["Acer Predator", 2], ["Asus ROG", 1], ["Lenovo Legion", 1]],
   member: true,
 };
 
-let customer2 = {
-  name: "Fadlila",
-  products: [
-    ["Acer Predator", 1],
-    ["Asus ROG", 3],
-    ["Lenovo Legion", 1],
-  ],
-  member: false,
-};
-
-let customer3 = {
-  name: "Jetly",
-  products: [["HP Omen", 2]],
-  member: true,
-};
-
-// TEST CASES
-console.log(shoppingTeros(customer1)); // Hai pelanggan setia Fajrin! Total Harga yang harus kamu bayar adalah Rp 92800000
-console.log(shoppingTeros(customer2)); // Hai Fadlila! Total Harga yang harus kamu bayar adalah Rp 118000000
-console.log(shoppingTeros(customer3)); // Hai pelanggan setia Jetly! Total Harga yang harus kamu bayar adalah Rp 32000000
+// Output: "Hai pelanggan setia Amir! Total Harga yang harus kamu bayar adalah Rp 71200000"
+const result = shoppingTeros(customer);
+console.log(result);

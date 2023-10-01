ad6c00216a35dd540097705c8647b87be6a7832a
AndaraAdisti <142381605+AndaraAdisti@users.noreply.github.com>
2023-10-01 12:25:42 +0700

    problem2 done
    problem2

--- Problem 2/index.js
+++ Problem 2/index.js
@@ -1,79 +1,67 @@
-function getProductsAmount(productsArr) {
-  // Write your code here
-  // EXPECTIATION
-  // Input from customer.products
-  // [
-  //   ["Asus ROG", 2],
-  //   ["Lenovo Legion", 3],
-  // ],
-  // Result
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
-}
-
-function getTotalPrice(productsObj) {
-  const listProduct = [
-    ["Asus ROG", 25000000],
-    ["Lenovo Legion", 22000000],
-    ["Zyrex 1945", 7000000],
-    ["HP Omen", 20000000],
-    ["Acer Predator", 21000000],
-  ];
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // {
-  //   "Asus ROG": 2,
-  //   "Lenovo Legion":3
-  // }
-  // Result
-  // 121000000 // NUMBER
-}
-
-function getDiscount(memberStatus, totalPrice) {
-  // Write your code here
-  // EXPECTIATION
-  // Input
-  // memberStatus = true
-  // totalPrice = 121000000
-  // Result
-  // 92800000 // NUMBER
-}
-
-function shoppingTeros(customer) {
-  // write your code here pakai function yg sudah dibuat diatas didalam sini
-  // EXPECTATION Return berupa string sesuai contoh dibawah
-  // apabila member maka panggil dia pelanggan setia
-}
-
-let customer1 = {
-  name: "Fajrin",
-  products: [
-    ["Asus ROG", 2],
-    ["Lenovo Legion", 3],
-  ],
-  member: true,
-};
-
-let customer2 = {
-  name: "Fadlila",
-  products: [
-    ["Acer Predator", 1],
-    ["Asus ROG", 3],
-    ["Lenovo Legion", 1],
-  ],
-  member: false,
-};
-
-let customer3 = {
-  name: "Jetly",
-  products: [["HP Omen", 2]],
-  member: true,
-};
-
-// TEST CASES
-console.log(shoppingTeros(customer1)); // Hai pelanggan setia Fajrin! Total Harga yang harus kamu bayar adalah Rp 92800000
-console.log(shoppingTeros(customer2)); // Hai Fadlila! Total Harga yang harus kamu bayar adalah Rp 118000000
-console.log(shoppingTeros(customer3)); // Hai pelanggan setia Jetly! Total Harga yang harus kamu bayar adalah Rp 32000000
+function getProductAmount(products) {
+    const productAmount = {};
+  
+    for (let i = 0; i < products.length; i++) {
+      const [productName, quantity] = products[i];
+      productAmount[productName] = (productAmount[productName] || 0) + quantity;
+    }
+  
+    return productAmount;
+  }
+  
+  function getTotalPrice(listProductAmount) {
+    let totalPrice = 0;
+  
+    for (const productName in listProductAmount) {
+      if (listProductAmount.hasOwnProperty(productName)) {
+        const quantity = listProductAmount[productName];
+        const productPrice = getProductPrice(productName);
+        totalPrice += productPrice * quantity;
+      }
+    }
+  
+    return totalPrice;
+  }
+  
+  function getDiscount(statusMember, totalPrice) {
+    if (statusMember) {
+      return totalPrice * 0.8; // 20% discount for members
+    } else {
+      return totalPrice;
+    }
+  }
+  
+  function getProductPrice(productName) {
+    // Menyediakan daftar harga produk
+    const productPrices = {
+      'Asus ROG': 25000000,
+      'Lenovo Legion': 22000000,
+      'HP Omen': 20000000,
+      'Acer Predator': 21000000,
+    };
+  
+    return productPrices[productName] || 0;
+  }
+  
+  function shoppingTeros(customer) {
+    const listProductAmount = getProductAmount(customer.products);
+    const totalPrice = getTotalPrice(listProductAmount);
+    const totalPriceAfterDiscount = getDiscount(customer.member, totalPrice);
+  
+    if (customer.member) {
+      return `Hai pelanggan setia ${customer.name}! Total Harga yang harus kamu bayar adalah Rp ${totalPriceAfterDiscount}`;
+    } else {
+      return `Hai ${customer.name}! Total Harga yang harus kamu bayar adalah Rp ${totalPriceAfterDiscount}`;
+    }
+  }
+  
+  // Contoh penggunaan
+  const customer = {
+    name: "Amir",
+    products: [["Acer Predator", 2], ["Asus ROG", 1], ["Lenovo Legion", 1]],
+    member: true
+  };
+  
+  const result = shoppingTeros(customer);
+  console.log(result); // Output: "Hai pelanggan setia Amir! Total Harga yang harus kamu bayar adalah Rp 71200000"
+  
